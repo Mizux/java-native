045ddd2ab2ddc7bb2693d6d3b7acc8fe48a42c61
Corentin Le Molgat <corentinl@google.com>
2021-08-30 15:13:24 +0200

    Rework java.cmake

--- cmake/java.cmake
+++ cmake/java.cmake
@@ -28,14 +28,30 @@ else()
   message(STATUS "Found Maven: ${MAVEN_EXECUTABLE}")
 endif()
 
+# Needed by java/CMakeLists.txt
+set(JAVA_PACKAGE org.mizux.javanative)
+set(JAVA_PACKAGE_PATH src/main/java/org/mizux/javanative)
+  set(JAVA_TEST_PATH src/test/java/org/mizux/javanative)
+set(JAVA_RESOURCES_PATH src/main/resources)
+if(APPLE)
+  set(NATIVE_IDENTIFIER darwin-x86-64)
+elseif(UNIX)
+  set(NATIVE_IDENTIFIER linux-x86-64)
+elseif(WIN32)
+  set(NATIVE_IDENTIFIER win32-x86-64)
+else()
+  message(FATAL_ERROR "Unsupported system !")
+endif()
+set(JAVA_NATIVE_PROJECT javanative-${NATIVE_IDENTIFIER})
+set(JAVA_PROJECT javanative-java)
+
 # Create the native library
 add_library(jnijavanative SHARED "")
 set_target_properties(jnijavanative PROPERTIES
   POSITION_INDEPENDENT_CODE ON)
 # note: macOS is APPLE and also UNIX !
 if(APPLE)
-  set_target_properties(jnijavanative PROPERTIES
-    INSTALL_RPATH "@loader_path")
+  set_target_properties(jnijavanative PROPERTIES INSTALL_RPATH "@loader_path")
   # Xcode fails to build if library doesn't contains at least one source file.
   if(XCODE)
     file(GENERATE
@@ -47,22 +63,6 @@ elseif(UNIX)
   set_target_properties(jnijavanative PROPERTIES INSTALL_RPATH "$ORIGIN")
 endif()
 
-# Needed by java/CMakeLists.txt
-set(JAVA_PACKAGE org.mizux.javanative)
-set(JAVA_PACKAGE_PATH src/main/java/org/mizux/javanative)
-set(JAVA_RESOURCES_PATH src/main/resources)
-if(APPLE)
-  set(NATIVE_IDENTIFIER darwin)
-elseif(UNIX)
-  set(NATIVE_IDENTIFIER linux-x86-64)
-elseif(WIN32)
-  set(NATIVE_IDENTIFIER win32-x86-64)
-else()
-  message(FATAL_ERROR "Unsupported system !")
-endif()
-set(JAVA_NATIVE_PROJECT javanative-${NATIVE_IDENTIFIER})
-set(JAVA_PROJECT javanative-java)
-
 # Swig wrap all libraries
 foreach(SUBPROJECT IN ITEMS Foo)
   add_subdirectory(${SUBPROJECT}/java)
@@ -72,66 +72,57 @@ endforeach()
 #################################
 ##  Java Native Maven Package  ##
 #################################
+set(JAVA_NATIVE_PROJECT_PATH ${PROJECT_BINARY_DIR}/java/${JAVA_NATIVE_PROJECT})
+file(MAKE_DIRECTORY ${JAVA_NATIVE_PROJECT_PATH}/${JAVA_RESOURCES_PATH}/${JAVA_NATIVE_PROJECT})
+
 configure_file(
   ${PROJECT_SOURCE_DIR}/java/pom-native.xml.in
-  ${PROJECT_BINARY_DIR}/java/pom-native.xml.in
+  ${JAVA_NATIVE_PROJECT_PATH}/pom.xml
   @ONLY)
-
-add_custom_command(
-  OUTPUT java/${JAVA_NATIVE_PROJECT}/pom.xml
-  DEPENDS ${PROJECT_BINARY_DIR}/java/pom-native.xml.in
-  COMMAND ${CMAKE_COMMAND} -E make_directory ${JAVA_NATIVE_PROJECT}
-  COMMAND ${CMAKE_COMMAND} -E copy ./pom-native.xml.in ${JAVA_NATIVE_PROJECT}/pom.xml
-  BYPRODUCTS
-  java/${JAVA_NATIVE_PROJECT}
-  WORKING_DIRECTORY java)
 
 add_custom_target(java_native_package
   DEPENDS
-  java/${JAVA_NATIVE_PROJECT}/pom.xml
-  COMMAND ${CMAKE_COMMAND} -E remove_directory src
-  COMMAND ${CMAKE_COMMAND} -E make_directory ${JAVA_RESOURCES_PATH}/${NATIVE_IDENTIFIER}
-  COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:Foo> ${JAVA_RESOURCES_PATH}/${NATIVE_IDENTIFIER}/
-  COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:jnijavanative> ${JAVA_RESOURCES_PATH}/${NATIVE_IDENTIFIER}/
+  ${JAVA_NATIVE_PROJECT_PATH}/pom.xml
+  COMMAND ${CMAKE_COMMAND} -E copy
+    $<TARGET_FILE:jnijavanative>
+    $<$<NOT:$<PLATFORM_ID:Windows>>:$<TARGET_SONAME_FILE:Foo>>
+    ${JAVA_RESOURCES_PATH}/${JAVA_NATIVE_PROJECT}/
   COMMAND ${MAVEN_EXECUTABLE} compile -B
   COMMAND ${MAVEN_EXECUTABLE} package -B
   COMMAND ${MAVEN_EXECUTABLE} install -B $<$<BOOL:${SKIP_GPG}>:-Dgpg.skip=true>
-  WORKING_DIRECTORY java/${JAVA_NATIVE_PROJECT})
+  BYPRODUCTS
+    ${JAVA_NATIVE_PROJECT_PATH}/target
+  WORKING_DIRECTORY ${JAVA_NATIVE_PROJECT_PATH})
 
 ##########################
 ##  Java Maven Package  ##
 ##########################
+set(JAVA_PROJECT_PATH ${PROJECT_BINARY_DIR}/java/${JAVA_PROJECT})
+file(MAKE_DIRECTORY ${JAVA_PROJECT_PATH}/${JAVA_PACKAGE_PATH})
+
 configure_file(
   ${PROJECT_SOURCE_DIR}/java/pom-local.xml.in
-  ${PROJECT_BINARY_DIR}/java/pom-local.xml.in
+  ${JAVA_PROJECT_PATH}/pom.xml
   @ONLY)
-
-add_custom_command(
-  OUTPUT java/${JAVA_PROJECT}/pom.xml
-  DEPENDS ${PROJECT_BINARY_DIR}/java/pom-local.xml.in
-  COMMAND ${CMAKE_COMMAND} -E make_directory ${JAVA_PROJECT}
-  COMMAND ${CMAKE_COMMAND} -E copy ./pom-local.xml.in ${JAVA_PROJECT}/pom.xml
-  BYPRODUCTS
-  java/${JAVA_PROJECT}
-  WORKING_DIRECTORY java)
 
 add_custom_target(java_package ALL
   DEPENDS
-  java/${JAVA_PROJECT}/pom.xml
+  ${JAVA_PROJECT_PATH}/pom.xml
   COMMAND ${CMAKE_COMMAND} -E copy ${PROJECT_SOURCE_DIR}/java/Loader.java ${JAVA_PACKAGE_PATH}/
   COMMAND ${MAVEN_EXECUTABLE} compile -B
   COMMAND ${MAVEN_EXECUTABLE} package -B
   COMMAND ${MAVEN_EXECUTABLE} install -B $<$<BOOL:${SKIP_GPG}>:-Dgpg.skip=true>
-  WORKING_DIRECTORY java/${JAVA_PROJECT})
+  BYPRODUCTS
+    ${JAVA_PROJECT_PATH}/target
+  WORKING_DIRECTORY ${JAVA_PROJECT_PATH})
 add_dependencies(java_package java_native_package)
 
 #################
 ##  Java Test  ##
 #################
 if(BUILD_TESTING)
-  set(TEST_PATH ${PROJECT_BINARY_DIR}/java/tests/Test)
-  set(JAVA_TEST_PATH src/test/java/org/mizux/javanative)
-  #file(MAKE_DIRECTORY ${TEST_PATH}/${JAVA_TEST_PATH})
+  set(TEST_PATH ${PROJECT_BINARY_DIR}/java/tests/javanative-test)
+  file(MAKE_DIRECTORY ${TEST_PATH}/${JAVA_TEST_PATH})
 
   file(COPY ${PROJECT_SOURCE_DIR}/java/FooTest.java
     DESTINATION ${TEST_PATH}/${JAVA_TEST_PATH})
@@ -144,7 +135,9 @@ if(BUILD_TESTING)
 
   add_custom_target(java_test_Test ALL
     DEPENDS ${TEST_PATH}/pom.xml
-    COMMAND ${MAVEN_EXECUTABLE} compile
+    COMMAND ${MAVEN_EXECUTABLE} compile -B
+    BYPRODUCTS
+      ${TEST_PATH}/target
     WORKING_DIRECTORY ${TEST_PATH})
   add_dependencies(java_test_Test java_package)
 
